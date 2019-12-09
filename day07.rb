f9196826d47b39357b60a80b7e201f087ff13187
Haseeb Qureshi <haseeb@haseebq.com>
2019-12-08 23:28:33 -0800

    Grind on day 7

--- day07.rb
+++ day07.rb
@@ -1,35 +1,40 @@
 require_relative 'utils/utils'
+require 'pry-nav'
 
 PHASE_SETTINGS = (0..4)
+# DEBUG = true
 file = File.read('input07.txt')
 file = '3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5'
-tape = file.chomp.split(',').map(&:to_i)
+tape = file.chomp.split(',').map(&:to_i).freeze
 
-class TwoInputReader < Struct.new(:input1, :input2)
+class MultiInputReader
+  def initialize(*inputs)
+    @inputs = inputs
+    @counter = 0
+  end
+
   def read
-    # raise "Read too many times!" if @read_more_than_twice
-    if !@second_read
-      @second_read = true
-      input1
-    else
-      @read_more_than_twice = true
-      input2
-    end
+    @counter += 1
+    @inputs[@counter] || @inputs.last # overflow!
   end
 end
 
-class WriterCapsule < Struct.new(:val, :written)
+class WriterCapsule < Struct.new(:val, :written, :halted)
   def write(x)
     self.written = true
     self.val = x
+  end
+
+  def halt
+    self.halted = true
   end
 end
 
 def phase_set(tape, a, b, c, d, e)
   capsule = WriterCapsule.new
-  IntcodeComputer.run_program(tape, TwoInputReader.new(a, 0), capsule)
+  IntcodeComputer.run_program(tape, MultiInputReader.new(a, 0), capsule)
   [b, c, d, e].each do |amp|
-    IntcodeComputer.run_program(tape, TwoInputReader.new(amp, capsule.val), capsule)
+    IntcodeComputer.run_program(tape, MultiInputReader.new(amp, capsule.val), capsule)
   end
   capsule.val
 end
@@ -41,24 +46,41 @@ def max_thruster(tape)
 end
 
 puts "Part 1"
-puts max_thruster(tape)
+# puts max_thruster(tape)
 
 puts "Part 2"
 NEW_PHASE_SETTINGS = (5..9)
 
+def initialize_computer(tape, val1, val2)
+  computer = IntcodeComputer.new(tape, MultiInputReader.new(val1, val2), WriterCapsule.new)
+  computer.execute_next_instruction until computer.output.written
+  computer
+end
+
+def continue_to_next_input(computer, new_input)
+  computer.input = MultiInputReader.new(new_input)
+  computer.execute_next_instruction until computer.output.written
+end
+
 def phase_set_with_feedback(tape, a, b, c, d, e)
-  capsule = WriterCapsule.new
-  IntcodeComputer.run_program(tape, TwoInputReader.new(a, 0), capsule)
-  [b, c, d, e].each do |amp|
-    capsule = WriterCapsule.new(capsule.val)
-    IntcodeComputer.run_program(tape, TwoInputReader.new(amp, capsule.val), capsule)
-  end
+  halted = false
+
+  computer_a = initialize_computer(tape, a, 0)
+  computer_b = initialize_computer(tape, b, computer_a.output.val)
+  computer_c = initialize_computer(tape, c, computer_b.output.val)
+  computer_d = initialize_computer(tape, d, computer_c.output.val)
+  computer_e = initialize_computer(tape, e, computer_d.output.val)
+
+  i = 0
   loop do
-    break unless capsule.written
-    [a, b, c, d, e].each do |amp|
-      capsule = WriterCapsule.new(capsule.val)
-      IntcodeComputer.run_program(tape, TwoInputReader.new(amp, capsule.val), capsule)
-    end
+    i += 1
+    # print i
+    break if computer_e.terminated
+    continue_to_next_input(computer_a, computer_e.output.val)
+    continue_to_next_input(computer_b, computer_a.output.val)
+    continue_to_next_input(computer_c, computer_b.output.val)
+    continue_to_next_input(computer_d, computer_c.output.val)
+    continue_to_next_input(computer_e, computer_d.output.val)
   end
   capsule.val
 end
@@ -68,5 +90,9 @@ def max_feedback_thruster(tape)
     phase_set_with_feedback(tape, a, b, c, d, e)
   end.max
 end
+
+puts phase_set_with_feedback(tape, 9, 8, 7, 6, 5)
+
+p "Oh snap"
 
 puts max_feedback_thruster(tape)
--- utils/utils.rb
+++ utils/utils.rb
@@ -5,103 +5,131 @@ IMMEDIATE = 1
 RELATIVE = 2
 
 class IntcodeComputer
+  attr_accessor :tape, :input, :output, :terminated
   def self.run_program(tape, input = Reader.new, output = Writer.new)
-    tape = Tape.new(tape.dup)
-    i = 0
-    shift = 0
-
-    p tape.tape if DEBUG
-
-    until i >= tape.tape.length
-      operand1 = tape[i + 1]
-      operand2 = tape[i + 2]
-      operand3 = tape[i + 3]
-
-      digits = tape[i].digits.reverse.tap do |digits|
-        digits.unshift(0) until digits.length == 5
-      end
-      opcode = digits[3] * 10 + digits[4]
-
-      mode1, mode2, mode3 = digits[2], digits[1], digits[0]
-
-      op1 = self.adjust_operator_for_mode(operand1, mode1, tape, shift)
-      op2 = self.adjust_operator_for_mode(operand2, mode2, tape, shift)
-      op3 = self.adjust_operator3_for_mode(operand3, mode3, tape, shift)
-
-      sleep 0.6 if DEBUG
-      p tape.tape[i..i + 3] if DEBUG
-      p "Op1: #{op1}, Op2: #{op2}, Op3: #{op3}" if DEBUG
-      binding.pry if PRY
-      case opcode
-      when 1 # add
-        puts "Adding #{op1} and #{op2} into slot #{op3}" if DEBUG
-        tape[op3] = op1 + op2
-        i += 4
-      when 2 # multiply
-        puts "Multiplying #{op1} and #{op2} into slot #{op3}" if DEBUG
-        tape[op3] = op1 * op2
-        i += 4
-      when 3 # gets
-        # Ugly hack here to make sure relative mode gets set correctly here
-        operand1 += shift if mode1 == RELATIVE
-        puts "Reading input into slot #{operand1}" if DEBUG
-        tape[operand1] = input.read
-        i += 2
-      when 4 # puts
-        puts "Writing #{op1}" if DEBUG
-        output.write(op1)
-        i += 2
-      when 5 # jump if true
-        puts "Jumping to #{op2} if #{op1} > 0" if DEBUG
-        if !op1.zero?
-          i = op2
-        else
-          i += 3
-        end
-      when 6 # jump if false
-        puts "Jumping to #{op2} if #{op1} == 0" if DEBUG
-        if op1.zero?
-          i = op2
-        else
-          i += 3
-        end
-      when 7 # less than?
-        puts "Placing whether #{op1} < #{op2} into slot #{op3}" if DEBUG
-        tape[op3] = op1 < op2 ? 1 : 0
-        i += 4
-      when 8 # equals?
-        puts "Placing whether #{op1} == #{op2} into slot #{op3}" if DEBUG
-        tape[op3] = op1 == op2 ? 1 : 0
-        i += 4
-      when 9 # add to relative base
-        puts "Adding #{op1} to the current shift value, which is #{shift}" if DEBUG
-        shift += op1
-        i += 2
-      when 99
-        break
-      else
-        raise "Unexpected opcode at index #{i}: #{opcode}, #{tape[i]}"
-      end
-    end
-    tape.tape
+    IntcodeComputer.new(tape, input, output).run_program
   end
 
-  def self.adjust_operator_for_mode(operand, mode, tape, shift)
-    raise "why is operand nil?" if operand.nil?
+  def initialize(tape, input, output)
+    @tape = Tape.new(tape.dup)
+    @input = input
+    @output = output
+    @i = 0
+    @shift = 0
+    @terminated = false
+  end
+
+  def run_program
+    p @tape.tape if DEBUG
+    until @i >= @tape.tape.length || @terminated
+      execute_next_instruction
+    end
+    @tape.tape
+  end
+
+  def execute_next_instruction
+    @operand1 = @tape[@i + 1]
+    @operand2 = @tape[@i + 2]
+    @operand3 = @tape[@i + 3]
+
+    digits = @tape[@i].digits.reverse.tap do |digits|
+      digits.unshift(0) until digits.length == 5
+    end
+    @opcode = digits[3] * 10 + digits[4]
+
+    @mode1, @mode2, @mode3 = digits[2], digits[1], digits[0]
+
+    sleep 0.6 if DEBUG
+    p @tape.tape[@i..@i + 3] if DEBUG
+    binding.pry if PRY
+
+    case @opcode
+    when 1 # add
+      op1 = adjust_operator_for_mode(@operand1, @mode1)
+      op2 = adjust_operator_for_mode(@operand2, @mode2)
+      op3 = adjust_address_operator_for_mode(@operand3, @mode3)
+      puts "Adding #{op1} and #{op2} into slot #{op3}" if DEBUG
+      @tape[op3] = op1 + op2
+      @i += 4
+    when 2 # multiply
+      op1 = adjust_operator_for_mode(@operand1, @mode1)
+      op2 = adjust_operator_for_mode(@operand2, @mode2)
+      op3 = adjust_address_operator_for_mode(@operand3, @mode3)
+      puts "Multiplying #{op1} and #{op2} into slot #{op3}" if DEBUG
+      @tape[op3] = op1 * op2
+      @i += 4
+    when 3 # gets
+      # Ugly hack here to make sure relative mode gets set correctly here
+      op1 = adjust_address_operator_for_mode(@operand1, @mode1)
+      puts "Reading input into slot #{@operand1}" if DEBUG
+      @tape[op1] = @input.read
+      @i += 2
+    when 4 # puts
+      op1 = adjust_operator_for_mode(@operand1, @mode1)
+      puts "Writing #{op1}" if DEBUG
+      @output.write(op1)
+      @i += 2
+    when 5 # jump if true
+      op1 = adjust_operator_for_mode(@operand1, @mode1)
+      op2 = adjust_operator_for_mode(@operand2, @mode2)
+      puts "Jumping to #{op2} if #{op1} > 0" if DEBUG
+      if !op1.zero?
+        @i = op2
+      else
+        @i += 3
+      end
+    when 6 # jump if false
+      op1 = adjust_operator_for_mode(@operand1, @mode1)
+      op2 = adjust_operator_for_mode(@operand2, @mode2)
+      puts "Jumping to #{op2} if #{op1} == 0" if DEBUG
+      if op1.zero?
+        @i = op2
+      else
+        @i += 3
+      end
+    when 7 # less than?
+      op1 = adjust_operator_for_mode(@operand1, @mode1)
+      op2 = adjust_operator_for_mode(@operand2, @mode2)
+      op3 = adjust_address_operator_for_mode(@operand3, @mode3)
+      puts "Placing whether #{op1} < #{op2} into slot #{op3}" if DEBUG
+      @tape[op3] = op1 < op2 ? 1 : 0
+      @i += 4
+    when 8 # equals?
+      op1 = adjust_operator_for_mode(@operand1, @mode1)
+      op2 = adjust_operator_for_mode(@operand2, @mode2)
+      op3 = adjust_address_operator_for_mode(@operand3, @mode3)
+      puts "Placing whether #{op1} == #{op2} into slot #{op3}" if DEBUG
+      @tape[op3] = op1 == op2 ? 1 : 0
+      @i += 4
+    when 9 # add to relative base
+      op1 = adjust_operator_for_mode(@operand1, @mode1)
+      puts "Adding #{op1} to the current shift value, which is #{@shift}" if DEBUG
+      @shift += op1
+      @i += 2
+    when 99 # halt
+      @output.halt if @output.respond_to?(:receive_halt)
+      @terminated = true
+    else
+      raise "Unexpected opcode at index #{@i}: #{@opcode}, #{@tape[@i]}"
+    end
+  end
+
+  def adjust_operator_for_mode(op, mode)
+    raise "why is operand nil?" if op.nil?
     case mode
-    when POSITION then tape[operand]
-    when IMMEDIATE then operand
-    when RELATIVE then tape[operand + shift]
+    when POSITION then @tape[op]
+    when IMMEDIATE then op
+    when RELATIVE then @tape[op + @shift]
     else raise "wtf is #{mode}"
     end
   end
 
-  def self.adjust_operator3_for_mode(operand, mode, tape, shift)
-    raise "why is operand nil?" if operand.nil?
+  def adjust_address_operator_for_mode(op, mode)
+    raise "why is operand nil?" if op.nil?
     case mode
-    when POSITION then operand
-    when IMMEDIATE then raise "Mode should not be immediate for param 3!"
-    when RELATIVE then operand + shift
+    when POSITION then op
+    when IMMEDIATE then raise "Mode should not be immediate for address operator!"
+    when RELATIVE then op + @shift
     else raise "wtf is #{mode}"
     end
   end
